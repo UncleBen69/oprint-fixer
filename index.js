8064df3156db1760583b0a95c7c4b19077824056
Vadim <valexeev30@gmail.com>
2022-08-08 13:42:30 +0100

    Kill Process

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const find = require('find-process');
 
-console.log("Test")
+// Make sure oprintware isn't running
+find('name', 'OPrint', true).then(function (list) {
+	console.log(list)
+
+	// If running kill
+	if(list.length !== 0){
+		console.log("Killing oprintware")
+		let PID = list[0].pid;
+
+		process.kill(PID, "SIGKILL");
+	};
+
+	console.log("after")
+
+	// Reg edit 
+	
+
+});
+
